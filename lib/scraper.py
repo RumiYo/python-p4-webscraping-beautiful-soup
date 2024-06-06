e8e811eae847b0c9248bdfc92a8fd8dace8649ef
RumiYo <yokoyamarumi@gmail.com>
2024-06-06 01:37:31 -0400

    Complete lab

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,16 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+
+print(doc.select('.heading-financier'))
+print(doc.select('.heading-financier')[0].contents)
+print(doc.select('.heading-60-black.color_black.mb-20'))
+
+courses = doc.select('.heading-60-black.colorblack.mb-20')
+for course in courses:
+    print(course.contents[0].strip())
+
+name = doc.select('.heading-60-black.color-black.mb-20')[0].name
+attr = doc.select('.heading-60-black.color-black.mb-20')[0].attrs
